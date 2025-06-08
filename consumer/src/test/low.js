d501b7810946b7e598bd891902add49f37c98aa3
Kashif Nazar <muhammad.kashif.nazar@gmail.com>
2025-06-08 18:01:28 +0100

    Basic structure

---
+++ consumer/src/test/low.js
@@ -0,0 +1,4 @@
+const plus = require('plus')
+
+console.log('This is index');
+console.log(plus(1, 2)); 
---
+++ plus/index.js
@@ -0,0 +1,6 @@
+function plus(a, b) {
+  console.log('This is plus');
+  return a + b;
+}
+
+module.exports = plus;
